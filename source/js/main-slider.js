fae9186b1c455042f91d4f5d5e7e0e0615e1607b
Alexey Lebedev <vorresti@ya.ru>
2020-06-20 20:49:00 +0300

    slider animation done

--- source/js/main-slider.js
+++ source/js/main-slider.js
@@ -1,19 +1,19 @@
 const sliderButtonBefore = document.getElementById('slider-button-before');
 const sliderButtonAfter = document.getElementById('slider-button-after');
-const sliderTumblerContainer = document.querySelector('.slider__tumbler-container');
+const sliderTumbler = document.querySelector('.slider__tumbler');
 const sliderItems = document.querySelectorAll('.slider__item');
 
 const sliderItemBefore = sliderItems[0];
 const sliderItemAfter = sliderItems[1];
 
 sliderButtonBefore.addEventListener('click', () => {
+  sliderTumbler.classList.toggle('slider__tumbler--position-after');
   sliderItemAfter.classList.remove('slider__item--active');
   sliderItemBefore.classList.add('slider__item--active');
-  sliderTumblerContainer.classList.toggle('slider__tumbler-container--tumbler-after');
 });
 
 sliderButtonAfter.addEventListener('click', () => {
+  sliderTumbler.classList.toggle('slider__tumbler--position-after');
   sliderItemBefore.classList.remove('slider__item--active');
   sliderItemAfter.classList.add('slider__item--active');
-  sliderTumblerContainer.classList.toggle('slider__tumbler-container--tumbler-after');
 })
