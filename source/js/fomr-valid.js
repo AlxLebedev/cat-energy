7a18be795946db8ce0107c963542dfa40263ddf0
Alexey Lebedev <vorresti@ya.ru>
2020-06-25 13:01:23 +0300

    form validation done - icons painting red or green

--- source/js/fomr-valid.js
+++ source/js/fomr-valid.js
@@ -1,17 +1,36 @@
 const invalidClassName = 'keyboard-input__field--invalid';
+const invalidIconClaccName = 'keyboard-input__icon--invalid';
+const validIconClassName = 'keyboard-input__icon--valid';
 const keyInputs = document.querySelectorAll('.keyboard-input__field');
+let currentIcon = null;
 
 keyInputs.forEach(input => {
-  input.addEventListener('input', () => {
+  input.addEventListener('input', (event) => {
+    if (event.target.nextElementSibling) {
+      currentIcon = event.target.nextElementSibling;
+    }
+
     if (input.validity.valid) {
+      if (currentIcon && currentIcon.classList.contains(invalidIconClaccName)) {
+        currentIcon.classList.remove(invalidIconClaccName);
+        currentIcon.classList.add(validIconClassName);
+      }
       if (input.classList.contains(invalidClassName)) {
         input.classList.remove(invalidClassName);
       }
     }
   });
 
-  input.addEventListener('focusout', () => {
+  input.addEventListener('focusout', (event) => {
+    if (event.target.nextElementSibling) {
+      currentIcon = event.target.nextElementSibling;
+    }
+
     if (input.validity.valid) {
+      if (currentIcon && currentIcon.classList.contains(invalidIconClaccName)) {
+        currentIcon.classList.remove(invalidIconClaccName);
+        currentIcon.classList.add(validIconClassName);
+      }
       if (input.classList.contains(invalidClassName)) {
         input.classList.remove(invalidClassName);
         return;
@@ -19,9 +38,16 @@ keyInputs.forEach(input => {
     return;
     }
     input.classList.add(invalidClassName);
+    if (currentIcon) {
+      currentIcon.classList.remove(validIconClassName);
+      currentIcon.classList.add(invalidIconClaccName);
+    }
   });
 
   input.addEventListener('invalid', () => {
     input.classList.add(invalidClassName);
+    if (input.nextElementSibling) {
+      input.nextElementSibling.classList.add(invalidIconClaccName);
+    }
   });
 });
