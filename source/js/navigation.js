f11a09888a2cb1c2dd9bb7f99e2deaec7d770588
Alexey Lebedev <vorresti@ya.ru>
2020-06-25 15:03:02 +0300

    JS comments fixed

--- source/js/navigation.js
+++ source/js/navigation.js
@@ -7,12 +7,10 @@
   navButton.addEventListener('click', () => {
     if (navList.classList.contains('navigation__list--closed')) {
       navList.classList.remove('navigation__list--closed');
-      navButton.classList.remove('navigation__toggle-button--menu-open');
-      navButton.classList.add('navigation__toggle-button--menu-close');
-      return;
+      navButton.classList.add('navigation__toggle-button--close');
+    } else {
+      navList.classList.add('navigation__list--closed');
+      navButton.classList.remove('navigation__toggle-button--close');
     }
-    navList.classList.add('navigation__list--closed');
-    navButton.classList.remove('navigation__toggle-button--menu-close');
-    navButton.classList.add('navigation__toggle-button--menu-open');
   });
 }
