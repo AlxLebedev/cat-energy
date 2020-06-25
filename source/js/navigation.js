b2407fd58c9940529cf684778fa7b78d3613785a
Alexey Lebedev <vorresti@ya.ru>
2020-06-25 18:06:47 +0300

    nav menu open/close logic changed

--- source/js/navigation.js
+++ source/js/navigation.js
@@ -5,11 +5,11 @@
   navList.classList.remove('navigation__list--noJS');
 
   navButton.addEventListener('click', () => {
-    if (navList.classList.contains('navigation__list--closed')) {
-      navList.classList.remove('navigation__list--closed');
+    if (!navList.classList.contains('navigation__list--opend')) {
+      navList.classList.add('navigation__list--opend');
       navButton.classList.add('navigation__toggle-button--close');
     } else {
-      navList.classList.add('navigation__list--closed');
+      navList.classList.remove('navigation__list--opend');
       navButton.classList.remove('navigation__toggle-button--close');
     }
   });
